531f02a7f913072c525ce7669610fc080e2c0962
ateerayut <ateerayut@gmail.com>
2021-10-06 09:36:31 +0700

    Add project files.

---
+++ RelationList/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿
+namespace RelationList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtConnectionString = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtConnectionString
+            // 
+            this.txtConnectionString.Location = new System.Drawing.Point(163, 39);
+            this.txtConnectionString.Name = "txtConnectionString";
+            this.txtConnectionString.Size = new System.Drawing.Size(950, 20);
+            this.txtConnectionString.TabIndex = 0;
+            this.txtConnectionString.Text = "Data Source=ptt-ctrmdb-t07.ptt.corp;Initial Catalog=smiletradedb_ERPLot1_Before;P" +
+    "ersist Security Info=True;User ID=ctrm_accnt;Password=P@ssw0rd";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(66, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Connection String";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(59, 121);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(214, 524);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Table";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1130, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "All Table";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(307, 95);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(412, 602);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(304, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Stored/Function/View";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(788, 95);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.Size = new System.Drawing.Size(436, 602);
+            this.dataGridView2.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(785, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Field";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(59, 651);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(214, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(59, 677);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(214, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Table Name Cross Database";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(59, 98);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(133, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(198, 95);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Filter";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1311, 739);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtConnectionString);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtConnectionString;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ RelationList/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RelationList
+{
+    public partial class Form1 : Form
+    {
+        private string strTableName = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.listBox1.Items.Clear();
+            using (SqlConnection connection = new SqlConnection(txtConnectionString.Text))
+            {
+
+                connection.Open();
+                var strSQL = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE' ORDER BY TABLE_NAME";
+                SqlCommand cmd = new SqlCommand(strSQL, connection);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+
+                    this.listBox1.Items.Add(reader["TABLE_NAME"].ToString());
+
+                }
+
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+            strTableName = listBox1.Text;
+            using (SqlConnection connection = new SqlConnection(txtConnectionString.Text))
+            {
+
+                connection.Open();
+                var strSQL = string.Format(@"SELECT o.Name, o.type
+                                    FROM sys.sql_modules m
+                                    INNER JOIN sys.objects o
+                                    ON o.object_id = m.object_id
+                                    WHERE m.definition like '%{0}%'
+                                    order by o.Name ASC", strTableName);
+                SqlCommand cmd = new SqlCommand(strSQL, connection);
+                var dtSet = new DataSet();
+                var dtAdap = new SqlDataAdapter(strSQL, connection);
+                dtAdap.Fill(dtSet, "stroedName");
+                this.dataGridView1.AutoGenerateColumns = true;
+                this.dataGridView1.DataSource = dtSet;
+                this.dataGridView1.DataMember = "stroedName";
+
+
+            }
+            Cursor.Current = Cursors.Default;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+       
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            strTableName = textBox1.Text;
+            using (SqlConnection connection = new SqlConnection(txtConnectionString.Text))
+            {
+
+                connection.Open();
+                var strSQL = string.Format(@"SELECT  o.Name, o.type
+                                    FROM sys.sql_modules m
+                                    INNER JOIN sys.objects o
+                                    ON o.object_id = m.object_id
+                                    WHERE m.definition like '%{0}%'
+                                    order by o.Name ASC", strTableName);
+                SqlCommand cmd = new SqlCommand(strSQL, connection);
+                var dtSet = new DataSet();
+                var dtAdap = new SqlDataAdapter(strSQL, connection);
+                dtAdap.Fill(dtSet, "stroedName");
+                this.dataGridView1.AutoGenerateColumns = true;
+                this.dataGridView1.DataSource = dtSet;
+                this.dataGridView1.DataMember = "stroedName";
+
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.listBox1.Items.Clear();
+            using (SqlConnection connection = new SqlConnection(txtConnectionString.Text))
+            {
+
+                connection.Open();
+                var strSQL = string.Format("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE' AND  TABLE_NAME LIKE '%{0}%' ORDER BY TABLE_NAME", textBox2.Text.Trim()) ;
+                SqlCommand cmd = new SqlCommand(strSQL, connection);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+
+                    this.listBox1.Items.Add(reader["TABLE_NAME"].ToString());
+
+                }
+
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+            // var storedName = dataGridView1.SelectedCells[0].Value.ToString();
+            this.dataGridView2.DataSource = null;
+            int rowindex = dataGridView1.CurrentCell.RowIndex;
+            int columnindex = dataGridView1.CurrentCell.ColumnIndex;
+
+            var storedName = dataGridView1.Rows[rowindex].Cells["Name"].Value.ToString();
+
+            using (SqlConnection connection = new SqlConnection(txtConnectionString.Text))
+            {
+
+                connection.Open();
+                var strSQL = string.Format(@"SELECT DISTINCT
+                                    referenced_entity_name AS table_name
+                                    , referenced_minor_name as column_name
+                                FROM sys.dm_sql_referenced_entities ('dbo.{0}', 'OBJECT') WHERE referenced_entity_name = '{1}'
+                                order by table_name
+                            ", storedName, strTableName);
+                SqlCommand cmd = new SqlCommand(strSQL, connection);
+                var dtSet = new DataSet();
+                var dtAdap = new SqlDataAdapter(strSQL, connection);
+                dtAdap.Fill(dtSet, "tableName");
+                this.dataGridView2.AutoGenerateColumns = true;
+                this.dataGridView2.DataSource = dtSet;
+                this.dataGridView2.DataMember = "tableName";
+
+
+            }
+            Cursor.Current = Cursors.Default;
+        }
+    }
+}
